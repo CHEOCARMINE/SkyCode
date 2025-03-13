a50c392dfbe4ba958ed89922d66871135a07bdab
Luis Cajun <luiscajun96@gmail.com>
2025-03-13 15:03:08 -0600

    Permite al alumno consultar su historial académico, incluyendo calificaciones por cuatrimestre, materias aprobadas, reprobadas y el avance total de la carrera mediante un porcentaje

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from routes import academic_bp
 from functions.auth.login import auth_bp as login_bp
 from flask_login import LoginManager
 from models import Usuario
+from routes import alumno_progress_bp 
 
 def create_app(config_name="development"):
     """
@@ -38,6 +39,7 @@ def create_app(config_name="development"):
     # Registra los blueprints
     app.register_blueprint(academic_bp)
     app.register_blueprint(login_bp)
+    app.register_blueprint(alumno_progress_bp)
 
     # Manejo del error 413 (Request Entity Too Large)
     @app.errorhandler(413)
